dbee416868058512d7d81cdcd57527f9177661b8
roshni kashyap <kashyap.rosh77@gmail.com>
2015-06-12 12:46:26 +0530

    pushed people strong's mailer

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"> </script>
-	<script src="assets/js/script.js"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,18 +9,13 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="icon" sizes="144x144" type="image/png" href="assets/images/fav.png">
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=BlackOpsOne|Playball|RobotoSlab">
-<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-<style>
-#demo1 {
-	color:white;
-}
+<link rel="stylesheet" type="text/css" href="assets/css/home.css">
 </head>
-</style>
   <body>
   <div class="container">
   <!-- Static navbar -->
@@ -39,41 +32,40 @@
           </div>
           <div id="navbar" class="navbar-collapse collapse">
                        <ul class="nav navbar-nav navbar-right">
-             			<!--sign in section-->
+                  <!--sign in section-->
 <li>
- <form  name= "n2" action="signin.php"  method= "post">
+ <form  name= "n2" action="signin1.php" method= "post">
 <div class="form-group has-warning has-feedback">
 <label for="element-1" class="control-label">E-Mail</label>
-<input type="email" name= "email1" id="email1" required class="form-control"   placeholder="Enter a valid e-mail address">
+<input type="email" name= "email" class="form-control" required  placeholder="Enter a valid e-mail address">
 <span class="glyphicon glyphicon-user form-control-feedback"></span><!-- with form control feedback glyphicon inside the textfields-->
 </div></li>
 <li>
 <div class="form-group has-warning has-feedback">
 <label for="element-2" class="control-label">Password</label>
-<input type="password"  name= "password1" id="password1" required class="form-control"   placeholder="Enter your password">
- <p id="demo1"></p>
+<input type="password"  name= "password"  class="form-control"  required  placeholder="Enter your password">
 <span class="glyphicon glyphicon-asterisk form-control-feedback"></span>
 </div>
 </li>
-<li><div class="btn-toolbar "> <input type="submit" value="Sign in" id= "followbtn1" class="btn btn-primary btn-md"></div></form>
+<li><div class="btn-toolbar "> <input type="submit" value="Sign in" class="btn btn-primary btn-md"></div></form>
 <!-- controls grid system maintaining gap between columns-->
 </li>
-                </ul>
+ </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
       </nav> </div>
       <!-- Main component for a primary marketing message or call to action -->
     <div class="jumbotron">
  <div class="row">
-	 <div class="col-xs-12 col-md-6">
+   <div class="col-xs-12 col-md-6">
        <img src="assets/images/back1.jpg" class="img-responsive" alt="SOCIAL"></div>
          <div class="col-xs-12 col-md-6">
-		 <div class="row">
-		 <div class="panel panel-default">
+     <div class="row">
+     <div class="panel panel-default">
     <div class="panel-heading">Sign Up</div>
     <div class="panel-body">
-	<form class="register" name= "n1"  method="post" id= "followform" action="signup.php">
-	<div class="row"><div class="col-xs-12">
+  <form class="register" name= "n1"  method="post" id= "followform" action="signup1.php">
+  <div class="row"><div class="col-xs-12">
 <div class="form-group has-warning has-feedback">
 <label for="element-1" class="control-label">First Name</label>
 <input type="text" class="form-control"  required   name="fname" id="fname" placeholder="Enter your First name">
@@ -95,23 +87,22 @@
 <div class="form-group has-warning has-feedback">
 <label for="element-1" class="control-label">Password</label>
   <input type="password" class="form-control"    required name="password" id="password" placeholder="Password" >
- <p id="demo"></p>
 <span class="glyphicon glyphicon-asterisk form-control-feedback"></span><!-- with form control feedback glyphicon inside the textfields-->
 </div></div></div> 
 <div class="row"><div class="col-xs-12">
- <input type="submit"  id= "followbtn" class="btn btn-success btn-md" value="Create Account" onclick="$('#loading').show();"/>
-<div id="loading" style="display:none;"><img src='assets/images/pre.gif' height="50px" width="50px"/></div>
+ <input type="submit"  id= "followbtn" class="btn btn-success btn-md" value="Create Account">
         </form>
-		</div>
+    </div>
   </div>
 </div></div></div></div></div></div>
+
   <!-- /container -->
  <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
- <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.js"></script>
+ <script src="assets/js/jquery-2.1.1.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
   </body>
 </html>
