b50b0462e110136b463e9c6469f3f10087e8f73d
yashi-goyal <yashi.goyal777@gmail.com>
2015-06-25 16:03:11 +0530

    jpeg slicer

---
+++ jpeg slicer/upload_example.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> 
+<title>Example Page for JPEG SLICER -</title> 
+</head> 
+
+<body> 
+<form method="post" name="post_form" enctype="multipart/form-data" action="uploaded_example.php"> 
+<input type="hidden" name="MAX_FILE_SIZE" value="1000000" /> 
+<input type="file" name="image" /> 
+<input type="submit" name="submit" value="Upoad" /> 
+</form> 
+</body> 
+</html> 
